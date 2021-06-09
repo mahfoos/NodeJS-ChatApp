f13ad3508f21f0075514e4a22548dc0ba4720393
Mahfooz <umahfoosahamed@gmail.com>
2021-06-09 12:09:41 +0530

    added get

--- server.js
+++ server.js
@@ -3,4 +3,25 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname ))
-app.listen(3000)
+
+
+var messages = [
+    {
+        name:"Mahfoos",
+        message: "hi"
+    },
+
+    {
+        name:"Ahamed",
+        message: "Hello"
+    }
+]
+
+app.get('/messages', (req, res) => {  // call back
+    res.send(messages)
+} )   
+
+var server = app.listen(3000, () => {
+    console.log('Server is listening on port ', server.address().port)
+})
+
